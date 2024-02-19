34a99ad176453d90f2637ac9bd7c96956927af82
sinodeveloper <shandawei@me.com>
2024-02-20 02:18:42 +0800

    add port support

--- src/bin/yzb-vsc-win.ps1
+++ src/bin/yzb-vsc-win.ps1
@@ -4,14 +4,19 @@
 
 $user = $args[0]
 $serverHost = $args[1]
-
+$port = $args[2]
 # check the arg must be not null
 
 if ($null -eq $user -or $null -eq $serverHost) {
-    Write-Output "Usage: yzb-vsc-win.ps1 <user> <serverHost>"
+    Write-Output "Usage: yzb-vsc-win.ps1 <user> <serverHost> <port>"
     exit 1
 }
 
-$USER_AT_HOST = "$user@$serverHost"
+if ($null -eq $port) {
+    $port = 22
+} 
+
+$USER_AT_HOST = "$USER_AT_HOST -p $port"
+
 $PUBKEYPATH = "$HOME\.ssh\id_rsa.pub"
-$pubKey = (Get-Content "$PUBKEYPATH" | Out-String); ssh "$USER_AT_HOST" "mkdir -p ~/.ssh && chmod 700 ~/.ssh && echo '${pubKey}' >> ~/.ssh/authorized_keys && chmod 600 ~/.ssh/authorized_keys"
+$pubKey = (Get-Content "$PUBKEYPATH" | Out-String); ssh "$USER_AT_HOST"  "mkdir -p ~/.ssh && chmod 700 ~/.ssh && echo '${pubKey}' >> ~/.ssh/authorized_keys && chmod 600 ~/.ssh/authorized_keys"
